664e86c1272ac0de39a37f37604182d2954056ee
burinigomesjoaopedro-lab <burinigomesjoaopedro@gmail.com>
2026-01-28 18:51:37 -0300

    Criar page.js

---
+++ app/page.js
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main style={{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      fontFamily: 'Arial'
+    }}>
+      <h1>🏋️ Fitness App</h1>
+      <p>Criado 100% pelo iPhone 🚀</p>
+    </main>
+  )
+}
